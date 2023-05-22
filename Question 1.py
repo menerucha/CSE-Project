f2f727fbb92060c2d37786def9a810bc3d2e2098
Rucha Mene <130368638+menerucha@users.noreply.github.com>
2023-05-22 20:52:11 +0530

    Add files via upload

---
+++ Question 1.py
@@ -0,0 +1,16 @@
+#Instructions for all code as comments
+#start of the program
+#take input from the user
+A = int(input("Enter the first number: "))
+B = int(input("Enter the second number: "))
+
+#calculate the sum of A and B
+S = A + B
+
+#display the result
+print("The sum of", A, "and", B, "is", S)
+
+#end of the program
+
+for i in range(10):
+    pass
